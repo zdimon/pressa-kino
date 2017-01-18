041db91351a0097ab9e8cdf63294d5f3121b0987
zdimon <zdimon77@gmail.com>
2017-01-18 15:34:13 +0200

    auto commit

--- kino/settings.py
+++ kino/settings.py
@@ -94,5 +94,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+from local import *
+
+
 MEDIA_URL = '/media/'
