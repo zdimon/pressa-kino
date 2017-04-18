2fc72dcc37027ea768362d19edd8ccf8590ec801
zdimon <zdimon@example.com>
2017-04-18 18:49:13 +0300

    auto commit

--- design/widget/widget.js
+++ design/widget/widget.js
@@ -3,31 +3,39 @@
     
       $("#wg_content .number").click(function(evt){
          $('#wg_content li').removeClass('active');
+         $('#wg_content .item').removeClass('active');
          $(evt.target).addClass('active');
-      }); 
+         activate_tab();
+      });
+      
+      activate_tab = function(){
+        var indx =  $('#wg_content li.active').index();
+        $('#item-'+indx).addClass('active');
+        
+      }
     
       $("#wg_content .arrow").click(function(evt){
-        
-            
-             if($(evt.target).hasClass('arrow_forward')){
+             var clicked = $(evt.target);
+             
+             if(clicked.hasClass('arrow_forward')){
                 var nxt = $('#wg_content li.active').next();
-                
+                 
                 if(nxt.hasClass('number')){
                     $('#wg_content li').removeClass('active');
+                    $('#wg_content .item').removeClass('active');
                     nxt.click();
-                }
-                
-                
+                    activate_tab();
+                }             
              }
              
-             if($(evt.target).hasClass('arrow_backward')){
+             if(clicked.hasClass('arrow_backward')){
                 var prev = $('#wg_content li.active').prev();
                 if(prev.hasClass('number')){
                     $('#wg_content li').removeClass('active');
+                    $('#wg_content .item').removeClass('active');
                     prev.click();
-                }                
-                           
-                
+                    activate_tab();
+                }                        
              }             
             
         
