8f501b887b2ec9b3924a1c91c45b0e50d6604c5a
zdimon <zdimon@example.com>
2017-06-01 16:11:26 +0300

    auto commit

---
+++ main/migrations/0015_auto__add_message.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Message'
+        db.create_table(u'main_message', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=250)),
+            ('text', self.gf('ckeditor.fields.RichTextField')(null=True, blank=True)),
+            ('in_menu', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'main', ['Message'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Message'
+        db.delete_table(u'main_message')
+
+
+    models = {
+        u'main.blog': {
+            'Meta': {'object_name': 'Blog'},
+            'cropping': (u'django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'shorttext': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'text': ('ckeditor.fields.RichTextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'main.festival': {
+            'Meta': {'object_name': 'Festival'},
+            'alias': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'text': ('ckeditor.fields.RichTextField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        u'main.film': {
+            'Meta': {'object_name': 'Film'},
+            'code': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'cropping': (u'django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'festival': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['main.Festival']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'release_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'shorttext': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'text': ('ckeditor.fields.RichTextField', [], {'null': 'True', 'blank': 'True'}),
+            'video': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'main.message': {
+            'Meta': {'object_name': 'Message'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'in_menu': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'text': ('ckeditor.fields.RichTextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'main.news': {
+            'Meta': {'object_name': 'News'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'short_text': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'text': ('ckeditor.fields.RichTextField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        u'main.page': {
+            'Meta': {'object_name': 'Page'},
+            'alias': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'code': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'in_menu': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'text': ('ckeditor.fields.RichTextField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'video': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'main.videos': {
+            'Meta': {'object_name': 'Videos'},
+            'code': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'film': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['main.Film']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'video': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'main.vote': {
+            'Meta': {'object_name': 'Vote'},
+            'cnt': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'obj': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'score': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        }
+    }
+
+    complete_apps = ['main']
